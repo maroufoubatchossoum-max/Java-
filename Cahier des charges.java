aa90978054dead49c4f635e298b98305833461bb
maroufoubatchossoum-max <maroufoubatchossoum@gmail.com>
2026-03-01 23:53:10 +0100

    Create Cahier des charges.java

---
+++ Cahier des charges.java
@@ -0,0 +1,132 @@
+‎MODÈL DE PRÉSENTATION DU PROJET 
+
+1️⃣ PRÉSENTATION DU PROJET 
+‎
+‎1) NOM DU PROJET :  
+‎
+‎Mise en place d’une plateforme de gestion de présence des enseignants au CEG 3 NATITINGOU.
+‎
+‎2) STRUCTURE OU INSTITUTION
+‎
+‎Dans le cas de mémoire pour l'obtention de la licence en Mathématiques et Informatique.
+‎
+‎3) RESPONSABLE DU PROJET :
+‎
+‎BATCHOSSOUM Maroufou 
+‎
+‎
+‎2️⃣ CONTEXTE ET JUSTIFICATION
+‎
+‎1)  CONTEXTE 
+‎
+‎Lors de mon stage au CEG 3 Natitingou, j’ai constaté un conflit entre un enseignant et le censeur concernant la présence de  l'enseignant à un cours précédent.
+‎Le censeur affirmait que le professeur était absent, tandis que le professeur soutenait qu’il était présent.
+‎Cette situation a créé une incompréhension et un climat de tension entre les deux parties.
+‎
+‎2)  JUSTIFICATION
+‎
+‎Comment éviter les conflits liés à la vérification de la présence des enseignants en classe ?
+‎
+‎3️⃣  OBJECTIFS DU PROJET
+‎
+‎1) OBJECTIF GÉNÉRAL 
+‎
+‎Mettre en place une plateforme numérique permettant d’assurer une gestion fiable, transparente et efficace de la présence des enseignants afin de prévenir les conflits administratifs au sein du CEG 1 Natitingou.
+‎
+‎2) OBJECTIFS SPÉCIFIQUES
+‎
+‎🔹Permettre aux enseignants d’enregistrer leur présence en temps réel dès leur arrivée en salle de classe.
+‎🔹 Assurer l’envoi automatique d’une notification au censeur après chaque enregistrement de présence.
+‎🔹Permettre au censeur de vérifier et valider les présences enregistrées.
+‎🔹Envoyer une confirmation au professeur après validation de sa présence.
+‎🔹Mettre en place un système d’archivage numérique pour conserver l’historique des présences.
+‎🔹Réduire les malentendus et conflits liés aux absences ou retards.
+‎
+‎
+‎4️⃣ PUBLICS CIBLES 
+‎
+‎1) UTILISATEURS PRINCIPAUX 
+‎
+‎Les enseignants et le censeur 
+‎
+‎2)  UTILISATEURS SECONDAIRES 
+‎
+‎Le directeur, le sécréteur et le surveillant 
+‎
+‎3) PARTIE PRENANTE 
+‎🔹Le ministre de l'enseignement secondaire 
+‎🔹Les parents d'élèves 
+‎🔹Les développeurs informatiques 
+‎
+‎5️⃣ DESCRIPTION DES BESOINS 
+‎
+‎1)  BESOINS FONCTIONNELS DU SYSTÈME
+‎
+‎🔹Gérer des comptes utilisateurs  🔹Enregistrer la présence des professeurs 
+‎🔹Envoyer des notifications 
+‎🔹Valider ou rejeter les présences 
+‎🔹Consulter et archiver les enregistrements 
+‎🔹Sécuriser les données 
+‎
+‎2) BESOINS FONCTIONNELS NON FONCTIONNELS
+‎
+‎🔹Performance
+‎
+‎Le système doit répondre rapidement (moins de quelques secondes).
+‎Les notifications doivent être envoyées en temps réel.
+‎
+‎🔹 Sécurité
+‎
+‎Les données doivent être protégées par mot de passe.
+‎Chaque utilisateur ne peut accéder qu’à ses propres informations.
+‎Les données doivent être sauvegardées régulièrement.
+‎
+‎🔹 Fiabilité
+‎Le système doit fonctionner sans interruption pendant les heures de cours.
+‎Les données enregistrées ne doivent pas être perdues.
+‎
+‎🔹 Simplicité d’utilisation
+‎L’interface doit être simple et facile à comprendre.
+‎L’utilisation doit être possible sur téléphone et ordinateur.
+‎
+‎🔹 Compatibilité
+‎La plateforme doit fonctionner sur différents navigateurs.
+‎Elle doit être accessible avec une connexion internet standard.
+‎
+‎🔹 Maintenabilité
+‎Le système doit être facile à mettre à jour.
+‎Il doit permettre des améliorations futures.
+‎
+‎6️⃣ PÉRIMÈTRE DU PROJET 
+‎
+‎1)  INCLUS DANS LE PROJET 
+‎
+‎🔹 Développement de la plateforme 
+‎🔹 Gestion des utilisateurs 
+‎🔹 Gestion des présences 
+‎🔹 Archivage et rapports 
+‎🔹 Sécurisation des données 
+‎
+‎2) NON INCLUS DANS LE PROJET 
+‎
+‎🔹Gestion complète des emplois du temps 
+‎🔹Gestion des notes ou des résultats des élèves 
+‎🔹Suivi de la discipline des élèves   🔹Extension aux autres établissements 
+‎
+‎
+‎
+‎7️⃣ LIVRABLES ATTENDUS 
+‎
+‎🔹 Application web ou mobile 
+‎🔹 Interface pour les professeurs 
+‎🔹 Interface pour le censeur 
+‎🔹 Système de notification 
+‎🔹 Système de stockage de données 
+‎🔹 Historique des présences enregistré 
+‎🔹 Données sécurisées
+‎🔹 Description de l'architecture du système 
+‎🔹 Guide d'installation 
+‎🔹 Guide d'installation de la plateforme 
+‎🔹 Rapport de formation 
+‎🔹 Rapport de test .
+‎
